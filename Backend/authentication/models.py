b18235b58f2d9b96177489a5aad94d5570cfffbe
Khailas R <khailas303@gmail.com>
2022-05-13 12:15:20 +0530

    bug fix

--- Backend/authentication/models.py
+++ Backend/authentication/models.py
@@ -5,7 +5,6 @@ from django.utils import timezone
 
 
 class CustomUserManager(BaseUserManager):
-
     def create_user(self, username, password, **extra_fields):
         if not username:
             raise ValueError("Username field is required")
@@ -34,6 +33,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
