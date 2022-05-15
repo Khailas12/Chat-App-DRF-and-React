e40272ce501f133a6a17172fe3b352b0cf7c7a48
Khailas R <khailas303@gmail.com>
2022-05-15 13:50:25 +0530

    serializer updated

--- Backend/authentication/serializers.py
+++ Backend/authentication/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from .models import UserProfile
+from .models import UserProfile, CustomUser
+from message.serializers import GenericFileUploadSerializer
 
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+        
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer(read_only=True)
+    user_id = serializers.IntegerField(write_only=True)
+    profile_pic = GenericFileUploadSerializer(read_only=True)
+    profile_pic_id = serializers.IntegerField(write_only=True, required=False)
+    
     class Meta:
         model = UserProfile
         fields = '__all__'
